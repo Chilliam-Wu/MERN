dc8992231d06769b74da7fb54093866ef7927c32
Zhifan Wu <z392wu@uwaterloo.ca>
2022-02-20 16:42:22 -0500

    Finish Alert Setup

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('SUCCESS!');
+  };
+
+  return (
+    <Fragment>
+      <section className='container'>
+        <h1 className='large text-primary'>Sign In</h1>
+        <p className='lead'>
+          <i className='fas fa-user'></i> Sign intp Your Account
+        </p>
+        <form className='form' onSubmit={submitHandler}>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              required
+              onChange={(e) => changeHandler(e)}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              value={password}
+              minLength='6'
+              required
+              onChange={(e) => changeHandler(e)}
+            />
+          </div>
+          <input type='submit' className='btn btn-primary' value='Login' />
+        </form>
+        <p className='my-1'>
+          Don't have an account? <Link to='/register'>Sign up</Link>
+        </p>
+      </section>
+    </Fragment>
+  );
 }
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,105 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { setAlert } from '../../actions/alertActions';
+import Alert from '../layout/Alert';
 
-function Register() {
-  return <div>Register</div>;
+function Register({ setAlert }) {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const { name, email, password, confirmPassword } = formData;
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      console.log(formData);
+    }
+  };
+  return (
+    <Fragment>
+      <section className='container'>
+        <Alert />
+        <h1 className='large text-primary'>Sign Up</h1>
+        <p className='lead'>
+          <i className='fas fa-user'></i> Create Your Account
+        </p>
+        <form className='form' onSubmit={submitHandler}>
+          <div className='form-group'>
+            <input
+              type='text'
+              placeholder='Name'
+              name='name'
+              value={name}
+              required
+              onChange={(e) => changeHandler(e)}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              required
+              onChange={(e) => changeHandler(e)}
+            />
+            <small className='form-text'>
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              value={password}
+              minLength='6'
+              required
+              onChange={(e) => changeHandler(e)}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Confirm Password'
+              name='confirmPassword'
+              value={confirmPassword}
+              minLength='6'
+              required
+              onChange={(e) => changeHandler(e)}
+            />
+          </div>
+          <input type='submit' className='btn btn-primary' value='Register' />
+        </form>
+        <p className='my-1'>
+          Already have an account? <Link to='/login'>Sign In</Link>
+        </p>
+      </section>
+    </Fragment>
+  );
 }
 
-export default Register;
+// use prop-types to document the intended types
+// of properties passed to components
+Register.prototype = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+// connect() function connects a React component to a Redux store.
+// Inject setAlert without subscribing to the store
+// connect(null, { setAlert }) means we do not have mapStateToProps
+// we just need setAlert function
+export default connect(null, { setAlert })(Register);
